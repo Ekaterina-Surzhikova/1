59abba20de43e1d3f78029d39b756e06868e6839
Ekaterina-Surzhikova <k.surzhikova2006@gmail.com>
2025-06-14 22:36:44 +0500

    Update ИКМ.cpp

--- ИКМ.cpp
+++ ИКМ.cpp
@@ -1,24 +1,21 @@
-﻿#include <iostream>
-#include <fstream>
-#include <string>
 #include "Header.h"
 
-using namespace std;
-
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
     int choice;
     do {
-        showMenu();
+        showMenu();// Отображает главное меню программы
         if (!(cin >> choice)) {
-            clearInput();
+            clearInput(); //Очищает буфер ввода
             choice = 0;
         }
         clearInput();
 
         switch (choice) {
-        case 1: processFile(); break;
-        case 2: processManual(); break;
+        case 1: processFile(); break; // Обрабатывает ввод из файла
+        case 2: processManual(); break; // Обрабатывает ручной ввод пользователя
         case 3: cout << "Завершение.\n"; break;
         default: cout << "Неверный выбор!\n";
         }
